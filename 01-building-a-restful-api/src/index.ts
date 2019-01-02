bf8f3d7a4b97b94f2bbfa76d8b7457c1027156cf
Larry Botha <larry@fixate.it>
2019-01-03 00:33:42 +0200

    add .env

--- 01-building-a-restful-api/src/index.ts
+++ 01-building-a-restful-api/src/index.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
 import * as http from 'http';
+import * as url from 'url';
 
-const port = 3000;
+dotenv.config();
 
 const server: http.Server = http.createServer((_, res: http.ServerResponse) => {
   res.end('Hello world\n');
 });
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   // tslint:disable-next-line
