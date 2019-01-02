f2da6ed6ce956f7d2fcfb3301fc720a91a7ee44d
Larry Botha <larry@fixate.it>
2019-01-03 00:34:16 +0200

    get data from request

--- 01-building-a-restful-api/src/index.ts
+++ 01-building-a-restful-api/src/index.ts
@@ -4,10 +4,26 @@ import * as url from 'url';
 
 dotenv.config();
 
-const server: http.Server = http.createServer((_, res: http.ServerResponse) => {
-  res.end('Hello world\n');
-});
 const port = process.env.PORT || 3000;
+
+const server: http.Server = http.createServer(
+  (req: http.IncomingMessage, res: http.ServerResponse) => {
+    // parse the URL with querystring
+    const parsedUrl = url.parse(req.url, true);
+    const {pathname, query} = parsedUrl;
+    const trimmedPath = pathname.replace(/^\/+|\/$/g, '');
+    const method = req.method.toLowerCase();
+
+    res.end('Hello world\n');
+
+    // tslint:disable-next-line
+    console.log(`
+      pathname: ${trimmedPath},
+      method: ${method},
+      query: ${JSON.stringify(query)}
+    `);
+  }
+);
 
 server.listen(port, () => {
   // tslint:disable-next-line
