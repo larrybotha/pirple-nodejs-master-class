5202f775a51ac758b2665e7a79aa601c64c65e4d
Larry Botha <larry@fixate.it>
2019-01-09 00:11:16 +0200

    add safe parser

--- 01-building-a-restful-api/src/lib/helpers.ts
+++ 01-building-a-restful-api/src/lib/helpers.ts
@@ -3,19 +3,28 @@ import * as crypto from 'crypto';
 import config from '../config';
 
 type Hash = (s: string) => string | boolean;
-const hash: Hash = string => {
-  if (typeof string === 'string') {
-    const hash = crypto
+const hash: Hash = str => {
+  if (typeof str === 'string') {
+    const hashedString = crypto
       .createHmac('sha256', config.hashingSecret)
-      .update(string)
+      .update(str)
       .digest('hex');
 
-    return hash;
+    return hashedString;
   } else {
     return false;
   }
 };
 
-const helpers = {};
+type ParseJsonToObject = (s: string) => object;
+const parseJsonToObject: ParseJsonToObject = str => {
+  try {
+    return JSON.parse(str);
+  } catch (err) {
+    return {};
+  }
+};
 
-module.exports = helpers;
+const helpers = {hash, parseJsonToObject};
+
+export default helpers;
