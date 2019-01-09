3d527dd528e3d464856bf1b10850731f646f286a
Larry Botha <larry@fixate.it>
2019-01-09 23:51:34 +0200

    add remaining methods to users service

--- 01-building-a-restful-api/src/lib/services/users.ts
+++ 01-building-a-restful-api/src/lib/services/users.ts
@@ -1,15 +1,47 @@
 import dataLib from '../data';
 import helpers from '../helpers';
-import {exists, isOfType, minLength, trim} from '../validate';
+import {equals, exists, isOfType, minLength, trim} from '../validate';
 
 import {Handler, RequestData} from './types';
+
+const validateFirstName = (firstName: string) =>
+  [firstName]
+    .map(exists('First name is required'))
+    .map(isOfType('Must be a string', {types: ['string']}))
+    .map(trim())
+    .find(Boolean);
+const validateLastName = (lastName: string) =>
+  [lastName]
+    .map(exists('Last name is required'))
+    .map(isOfType('Must be a string', {types: ['string']}))
+    .map(trim())
+    .find(Boolean);
+const validatePhone = (phone: string) =>
+  [phone]
+    .map(exists('Phone is required'))
+    .map(isOfType('Must be a string', {types: ['string']}))
+    .map(minLength('Phone must be at least 10 chars', {length: 10}))
+    .map(trim())
+    .find(Boolean);
+const validatePassword = (password: string) =>
+  [password]
+    .map(exists('Password is required'))
+    .map(isOfType('Must be a string', {types: ['string']}))
+    .map(minLength('Password must be at least 8 chars', {length: 8}))
+    .map(trim())
+    .find(Boolean);
+const validateTos = (tos: string) =>
+  [tos]
+    .map(exists('TOS is required'))
+    .map(equals('TOS must be true', {value: 'true'}))
+    .find(Boolean);
 
 interface UserPostPayload {
   firstName: string;
   lastName: string;
   password: string;
   phone: string;
-  tosAgreement: boolean;
+  tosAgreement: string;
 }
 
 interface UserMethods {
@@ -21,65 +53,75 @@ interface UserMethods {
 }
 
 const userMethods: UserMethods = {
-  delete: (data, cb) => {},
-  get: (data, cb) => {},
+  delete: async ({pathname}, cb) => {
+    const [_, phone] = pathname.split('/');
+
+    try {
+      await dataLib.delete('users', phone);
+
+      cb(200);
+    } catch (err) {
+      cb(200);
+    }
+  },
+
+  get: async ({pathname}, cb) => {
+    const [_, phone] = pathname.split('/');
+
+    try {
+      const data = await dataLib.read('users', phone);
+
+      try {
+        const parsedData = helpers.parseJsonToObject(data);
+
+        cb(200, parsedData);
+      } catch (err) {
+        cb(500, err);
+      }
+    } catch (err) {
+      cb(404, err);
+    }
+  },
 
   // required data: fiestName, lastName, phone, password, tosAgreement
   post: async ({payload}, cb) => {
-    const firstName = [payload.firstName]
-      .map(exists('First name is required'))
-      .map(isOfType('Must be a string', {types: ['string']}))
-      .map(trim())
-      .find(Boolean);
-    const lastName = [payload.lastName]
-      .map(exists('Last name is required'))
-      .map(isOfType('Must be a string', {types: ['string']}))
-      .map(trim())
-      .find(Boolean);
-    const phone = [payload.phone]
-      .map(exists('Phone is required'))
-      .map(isOfType('Must be a string', {types: ['string']}))
-      .map(minLength('Phone must be at least 10 chars', {length: 10}))
-      .map(trim())
-      .find(Boolean);
-    const password = [payload.password]
-      .map(exists('Password is required'))
-      .map(isOfType('Must be a string', {types: ['string']}))
-      .map(minLength('Password must be at least 8 chars', {length: 8}))
-      .map(trim())
-      .find(Boolean);
-    const tosAgreement = [payload.tosAgreement]
-      .map(exists('TOS is required'))
-      .map(isOfType('Must be boolean', {types: ['boolean']}))
-      .find(Boolean);
+    const firstName = validateFirstName(payload.firstName);
+    const lastName = validateLastName(payload.lastName);
+    const phone = validatePhone(payload.phone);
+    const password = validatePassword(payload.password);
+    const tosAgreement = validateTos(payload.tosAgreement);
     const fields = [firstName, lastName, password, phone, tosAgreement];
     const invalidFields = fields.filter(
       field => Boolean(field) && Boolean(field.error)
     );
-    const isValid = invalidFields.length > 0;
+    const isValid = invalidFields.length === 0;
 
     if (isValid) {
+      const hashedPassword = helpers.hash(payload.password);
+
+      if (!hashedPassword) {
+        return cb(500, {Error: `Couldn't hash user's password`});
+      }
+
+      const userObject = {
+        firstName,
+        hashedPassword,
+        lastName,
+        phone,
+        tosAgreement,
+      };
+
       try {
-        await dataLib.read('users', payload.phone);
+        // make sure user doens't exist
+        await dataLib.read('users', phone);
+
+        cb(400, {Error: 'User exists'});
+        // if it errors, create the file
       } catch (err) {
-        const hashedPassword = helpers.hash(payload.password);
-
-        if (!hashedPassword) {
-          return cb(500, {Error: `Couldn't hash user's password`});
-        }
-
-        const userObject = {
-          firstName,
-          hashedPassword,
-          lastName,
-          phone,
-          tosAgreement,
-        };
-
         try {
           await dataLib.create('users', phone as any, userObject);
 
-          cb(200);
+          cb(201);
         } catch (err) {
           cb(500, err);
         }
@@ -89,7 +131,27 @@ const userMethods: UserMethods = {
     }
   },
 
-  put: (data, cb) => {},
+  put: async ({pathname, payload}, cb) => {
+    const [_, phone] = pathname.split('/');
+
+    try {
+      const data = await dataLib.read('users', phone);
+
+      try {
+        const parsedData = helpers.parseJsonToObject(data);
+        await dataLib.update('users', phone, {
+          ...parsedData,
+          ...payload,
+        });
+
+        cb(200);
+      } catch (err) {
+        cb(500, err);
+      }
+    } catch (err) {
+      cb(404, err);
+    }
+  },
 };
 
 export const users: Handler = (data, cb) => {
