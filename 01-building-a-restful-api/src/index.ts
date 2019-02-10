e403a9f4b06c355ee3a5ffe0fd785b895ba57649
Larry Botha <larry@fixate.it>
2019-02-10 18:05:41 +0200

    enable workers

--- 01-building-a-restful-api/src/index.ts
+++ 01-building-a-restful-api/src/index.ts
@@ -1,12 +1,12 @@
 import server from './lib/server';
-// import workers from './lib/workers';
+import workers from './lib/workers';
 
 const init = () => {
   // start the server
   server.init();
 
   // start the workers
-  // workers.init()
+  workers.init();
 };
 
 const app = {init};
