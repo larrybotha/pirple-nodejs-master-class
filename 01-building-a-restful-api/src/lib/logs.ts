5883210d00e35c24cc7e6a837fdcb2aa29cb5313
Larry Botha <larry@fixate.it>
2019-02-14 09:20:20 +0200

    add remaining log methods

--- 01-building-a-restful-api/src/lib/logs.ts
+++ 01-building-a-restful-api/src/lib/logs.ts
@@ -12,10 +12,24 @@ const asyncOpen = promisify(fs.open);
 const asyncWriteFile = promisify(fs.writeFile);
 const asyncReadFile = promisify(fs.readFile);
 const asyncReaddir = promisify(fs.readdir);
+const asyncTruncate = promisify(fs.truncate);
 const asyncGzip = (buf: zlib.InputType): Promise<Buffer> => {
   return new Promise((resolve, reject) => {
     zlib.gzip(buf, (err, res) => {
-      if (err) return reject(err);
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(res);
+    });
+  });
+};
+const asyncUnzip = (buf: zlib.InputType): Promise<Buffer> => {
+  return new Promise((resolve, reject) => {
+    zlib.unzip(buf, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
 
       return resolve(res);
     });
@@ -24,24 +38,28 @@ const asyncGzip = (buf: zlib.InputType): Promise<Buffer> => {
 
 const baseDir = path.join(__dirname, '../../../.logs');
 
-const getFilePath = (filename: string) => path.join(baseDir, `${filename}.log`);
+const getFilePath = (logId: string, isGzip?: boolean) => {
+  const ext = isGzip ? 'gz.b64' : 'log';
+
+  return path.join(baseDir, `${logId}.${ext}`);
+};
 
 /*
  * Append logs to a file, create a file if one doesn't exist
  */
-type Append = (filename: string, data: string) => Promise<string>;
-const append: Append = async (filename, data) => {
-  const file = getFilePath(filename);
+type Append = (logId: string, data: string) => Promise<string>;
+const append: Append = async (logId, data) => {
+  const filePath = getFilePath(logId);
   const dataToAppend = `${data}\n`;
 
   try {
-    const fileDescriptor = await asyncOpen(file, 'a');
+    const fileDescriptor = await asyncOpen(filePath, 'a');
     await asyncAppendFile(fileDescriptor, dataToAppend);
     await asyncClose(fileDescriptor);
 
     return dataToAppend;
   } catch (err) {
-    throw new err();
+    throw err;
   }
 };
 
@@ -50,14 +68,14 @@ const append: Append = async (filename, data) => {
  */
 type Compress = (logId: string, newFileId: string) => Promise<void>;
 const compress: Compress = async (logId, newFileId) => {
-  const sourceFileName = `${logId}.log`;
-  const destFileName = `${newFileId}.gz.b64`;
+  const sourceFileName = getFilePath(logId);
+  const destFileName = getFilePath(newFileId, true);
 
   try {
-    const data = await asyncReadFile(getFilePath(sourceFileName), 'utf8');
+    const data = await asyncReadFile(sourceFileName, 'utf8');
     const compressedData = await asyncGzip(data);
     // open file for writing
-    const fileDescriptor = await asyncOpen(getFilePath(destFileName), 'wx');
+    const fileDescriptor = await asyncOpen(destFileName, 'wx');
     // write to file
     await asyncWriteFile(fileDescriptor, compressedData.toString('base64'));
     // close file once we are done with it
@@ -68,16 +86,34 @@ const compress: Compress = async (logId, newFileId) => {
 };
 
 /*
- * Retrieve a list of log files
+ * Decompress the contents of a .gz.b64 file
+ */
+type Decompress = (fileId: string) => Promise<string>;
+const decompress: Decompress = async fileId => {
+  const file = getFilePath(fileId, true);
+
+  try {
+    const compressedStr = await asyncReadFile(file, 'utf8');
+    const inputBuffer = Buffer.from(compressedStr, 'base64');
+    const resultBuffer = await asyncUnzip(inputBuffer);
+
+    return resultBuffer.toString();
+  } catch (err) {
+    throw err;
+  }
+};
+
+/*
+ * Retrieve a list of log file ids
  *
  * @param includeCompressedLogs {boolean} - include compressed log files in result
- * @return string[] - an array of log ids
+ * @return logIds {string[]} - an array of log ids
  */
 type List = (includeCompressedLogs?: boolean) => Promise<string[]>;
 const list: List = async (includeCompressedLogs = false) => {
   try {
     const fileNames = await asyncReaddir(baseDir);
-    const logFileIds = fileNames
+    const logIds = fileNames
       .filter(
         fn =>
           /\.log$/.test(fn) || (includeCompressedLogs && /\.gz\.b64$/.test(fn))
@@ -85,15 +121,23 @@ const list: List = async (includeCompressedLogs = false) => {
       .map(fn => fn.replace(/\.log$/, ''))
       .map(fn => fn.replace(/\.gz\.b64/, ''));
 
-    return logFileIds;
+    return logIds;
   } catch (err) {
     throw err;
   }
 };
 
-type Truncate = () => Promise<void>;
-const truncate: Truncate = () => {};
+type Truncate = (logId: string) => Promise<void>;
+const truncate: Truncate = async logId => {
+  const filePath = getFilePath(logId);
 
-const logs = {append, compress, list, truncate};
+  try {
+    await asyncTruncate(filePath, 0);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const logs = {append, compress, decompress, list, truncate};
 
 export default logs;
