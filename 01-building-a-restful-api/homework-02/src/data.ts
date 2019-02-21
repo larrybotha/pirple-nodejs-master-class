c68f1bc0e8ff47e77d8c3545e366c3e267b639b3
Larry Botha <larry@fixate.it>
2019-02-21 09:46:22 +0200

    begin data files

---
+++ 01-building-a-restful-api/homework-02/src/data.ts
@@ -0,0 +1,46 @@
+import * as fs from 'fs';
+import {promisify} from 'util';
+
+const asyncOpen = promisify(fs.open);
+const asyncReadFile = promisify(fs.readFile);
+const asyncWriteFile = promisify(fs.writeFile);
+const asyncClose = promisify(fs.close);
+
+type Create = (filePath: string, data: any) => Promise<string>;
+const create: Create = async (filePath, data) => {
+  try {
+    const fileDescriptor = await asyncOpen(filePath, 'wx');
+
+    await asyncWriteFile(fileDescriptor, JSON.stringify(data), 'utf8');
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+type Read = (filePath: string) => Promise<any>;
+const read: Read = async filePath => {
+  try {
+    const fileDescriptor = await asyncOpen(filePath, 'r');
+    const resultBuffer = await asyncReadFile(fileDescriptor);
+
+    await asyncClose(fileDescriptor);
+
+    return JSON.parse(resultBuffer.toString());
+  } catch (err) {
+    throw err;
+  }
+};
+
+type Patch = () => Promise<string>;
+type Remove = () => Promise<string>;
+type Update = () => Promise<string>;
+
+export default {
+  create,
+  patch,
+  read,
+  remove,
+  update,
+};
