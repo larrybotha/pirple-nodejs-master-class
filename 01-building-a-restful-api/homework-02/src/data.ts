1087c3ea7604c495e57375acb9405cf2a6f23f41
Larry Botha <larry@fixate.it>
2019-02-22 19:46:45 +0200

    complete data lib

--- 01-building-a-restful-api/homework-02/src/data.ts
+++ 01-building-a-restful-api/homework-02/src/data.ts
@@ -1,10 +1,15 @@
 import * as fs from 'fs';
-import {promisify} from 'util';
+import {debuglog, promisify} from 'util';
 
+const debug = debuglog('data');
+
+const asyncAccess = promisify(fs.access);
 const asyncOpen = promisify(fs.open);
 const asyncReadFile = promisify(fs.readFile);
 const asyncWriteFile = promisify(fs.writeFile);
 const asyncClose = promisify(fs.close);
+const asyncFtruncate = promisify(fs.ftruncate);
+const asyncUnlink = promisify(fs.unlink);
 
 type Create = (filePath: string, data: any) => Promise<string>;
 const create: Create = async (filePath, data) => {
@@ -15,6 +20,7 @@ const create: Create = async (filePath, data) => {
 
     return data;
   } catch (err) {
+    debug(err);
     throw err;
   }
 };
@@ -26,20 +32,58 @@ const read: Read = async filePath => {
     const resultBuffer = await asyncReadFile(fileDescriptor);
     await asyncClose(fileDescriptor);
 
-    return JSON.parse(resultBuffer.toString());
+    return JSON.parse(resultBuffer.toString('utf8'));
   } catch (err) {
+    debug(err);
     throw err;
   }
 };
 
-type Patch = () => Promise<string>;
-type Remove = () => Promise<string>;
-type Update = () => Promise<string>;
+type Patch = (filePath: string, data: object) => Promise<any>;
+const patch: Patch = async (filePath, data) => {
+  try {
+    const fd = await asyncOpen(filePath, 'r+');
+    const fileData = await asyncReadFile(fd);
+    const newData = {...JSON.parse(fileData.toString()), ...data};
 
-export default {
-  create,
-  patch,
-  read,
-  remove,
-  update,
+    await asyncFtruncate(fd, 0);
+    await asyncWriteFile(fd, JSON.stringify(newData));
+    await asyncClose(fd);
+
+    return newData;
+  } catch (err) {
+    debug(err);
+    throw err;
+  }
 };
+
+type Remove = (filePath: string) => Promise<boolean>;
+const remove: Remove = async filePath => {
+  try {
+    await asyncAccess(filePath, fs.constants.F_OK);
+    await asyncUnlink(filePath);
+
+    return true;
+  } catch (err) {
+    debug(err);
+    return true;
+  }
+};
+
+type Update = (filePath: string, data: object) => Promise<any>;
+const update: Update = async (filePath, data) => {
+  try {
+    const fd = await asyncOpen(filePath, 'r+');
+
+    await asyncFtruncate(fd, 0);
+    await asyncWriteFile(fd, JSON.stringify(data));
+    await asyncClose(fd);
+
+    return data;
+  } catch (err) {
+    debug(err);
+    throw err;
+  }
+};
+
+export {create, patch, read, remove, update};
