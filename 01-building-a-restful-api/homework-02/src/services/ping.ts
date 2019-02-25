ad134eb1f3bbe8de4ad9b9743124b64a24b0cc30
Larry Botha <larry@fixate.it>
2019-02-25 22:15:43 +0200

    add ping service

--- 01-building-a-restful-api/homework-02/src/services/ping.ts
+++ 01-building-a-restful-api/homework-02/src/services/ping.ts
@@ -1,3 +1,17 @@
-const ping = {};
+import {Service} from '../types/services';
+
+import {createService, createSuccessResponse} from './utils';
+
+const pingMethods: Service = {
+  get: () => {
+    const response = createSuccessResponse({status: 200});
+
+    return response;
+  },
+};
+
+const allowedMethods = ['get'];
+
+const ping = createService(allowedMethods, pingMethods);
 
 export {ping};
