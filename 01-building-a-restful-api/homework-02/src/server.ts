49bac83ba7c5f62d12d257cbdc596e0234265b40
Larry Botha <larry@fixate.it>
2019-02-25 22:15:34 +0200

    pass rquests to router

--- 01-building-a-restful-api/homework-02/src/server.ts
+++ 01-building-a-restful-api/homework-02/src/server.ts
@@ -3,20 +3,24 @@ import * as http from 'http';
 import * as https from 'https';
 
 import {config} from './config';
+import {router} from './router';
 
 type Init = () => void;
 const init: Init = () => {
-  const httpServer = http.createServer();
+  const httpServer = http.createServer(router);
 
   httpServer.listen(config.http.port, () => {
     // tslint:disable-next-line
     console.log(`listening on ${config.http.port}`);
   });
 
-  const httpsServer = https.createServer({
-    cert: fs.readFileSync(config.https.sslCertPath),
-    key: fs.readFileSync(config.https.sslKeyPath),
-  });
+  const httpsServer = https.createServer(
+    {
+      cert: fs.readFileSync(config.https.sslCertPath),
+      key: fs.readFileSync(config.https.sslKeyPath),
+    },
+    router
+  );
 
   httpsServer.listen(config.https.port, () => {
     // tslint:disable-next-line
