3b925d06ffc2b440508c159f41f41ce0a59ef549
Larry Botha <larry@fixate.it>
2019-02-25 22:13:25 +0200

    read stream data for request payload, use writeHead to set status of response

--- 01-building-a-restful-api/homework-02/src/router.ts
+++ 01-building-a-restful-api/homework-02/src/router.ts
@@ -2,21 +2,33 @@ import * as http from 'http';
 import * as url from 'url';
 
 import {services} from './services';
+import {notFound} from './services/not-found';
 
 type Router = (req: http.IncomingMessage, res: http.ServerResponse) => void;
 const router: Router = (req, res) => {
   const {url: reqUrl, headers, method} = req;
   const {pathname, query} = url.parse(reqUrl, true);
   const serviceName = pathname.split('/').find(Boolean);
-  const service = services[serviceName];
+  const service = services[serviceName] || notFound;
+  let data: string;
 
-  res.setHeader('Content-type', 'application/json');
+  req.setEncoding('utf8');
 
-  if (service) {
-    service({headers, method, pathname, query});
-  } else {
-    services.notFound();
-  }
+  req.on('data', d => {
+    data = data + d;
+  });
+
+  req.on('end', () => {
+    const payload = data ? JSON.parse(data) : null;
+    const responseData = service(
+      {headers, method: method.toUpperCase(), pathname, query},
+      payload
+    );
+
+    res.setHeader('Content-type', 'application/json');
+    res.writeHead(responseData.status);
+    res.end(JSON.stringify(responseData));
+  });
 };
 
 export {router};
