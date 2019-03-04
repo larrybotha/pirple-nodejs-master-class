f613a2b9be6ab44af93121b5f7524f1bf94a4d1c
Larry Botha <larry@fixate.it>
2019-03-04 09:17:25 +0200

    use StringDecoder to parse request payload

--- 01-building-a-restful-api/homework-02/src/router.ts
+++ 01-building-a-restful-api/homework-02/src/router.ts
@@ -1,5 +1,6 @@
 import * as http from 'http';
 import * as url from 'url';
+import {StringDecoder} from 'string_decoder';
 
 import {safeStringify} from './helpers';
 import {services} from './services';
@@ -12,15 +13,17 @@ const router: Router = (req, res) => {
   const serviceName = pathname.split('/').find(Boolean);
   const service = services[serviceName] || notFound;
   let data: string;
+  const decoder = new StringDecoder('utf8');
+  let data = '';
 
   req.setEncoding('utf8');
 
   req.on('data', d => {
-    data = data + d;
+    data += decoder.write(d);
   });
 
   req.on('end', () => {
-    const requestPayload = data ? JSON.parse(data) : {};
+    data += decoder.end();
     const {payload, metadata} = service(
       {headers, method: method.toUpperCase(), pathname, query},
       requestPayload
