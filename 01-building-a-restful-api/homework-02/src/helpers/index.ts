e6a640a07c62d5323c01e467a2e0942746f0d7f2
Larry Botha <larry@fixate.it>
2019-03-04 09:18:13 +0200

    begin crypto helper

--- 01-building-a-restful-api/homework-02/src/helpers/index.ts
+++ 01-building-a-restful-api/homework-02/src/helpers/index.ts
@@ -1,10 +1,29 @@
-const safeStringify = (data: any) => {
+import * as crypto from 'crypto';
+
 const safeJSONParse = (data: any) => {
   try {
     return JSON.parse(data);
   } catch (err) {
     return {};
   }
+};
+
+type CreateHash = (str: string) => string;
+const createHash = (length = 10) => {};
+
+type CreateRandomString = (length: number) => string;
+const createRandomString: CreateRandomString = (length = 0) => {
+  const chars = 'abcdefghijklmnopsqrstuvxyz';
+  const charsLength = chars.length;
+  const randString = Array.from({length})
+    .map(() => {
+      const randInt = Math.floor(Math.random() * charsLength);
+
+      return chars[randInt];
+    })
+    .join('');
+
+  return randString;
 };
 
 export {safeJSONParse};
