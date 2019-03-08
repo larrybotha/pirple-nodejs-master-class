8e7596497ef21de861da8cfff60b58daea49d65d
Larry Botha <larry@fixate.it>
2019-03-08 09:05:59 +0200

    add delete and get methods for user, add tests

--- 01-building-a-restful-api/homework-02/src/services/users.ts
+++ 01-building-a-restful-api/homework-02/src/services/users.ts
@@ -7,9 +7,61 @@ import {hasErrors} from '../validations';
 import {validateEmail, validatePassword} from '../validations/users';
 import {createErrorResponse, createService} from './utils';
 
-const BASE_DIR = 'user';
+const BASE_DIR = 'users';
 
 const userMethods: Service<User> = {
+  /*
+   * Delete a user
+   *
+   * /users/:email
+   */
+  delete: async ({pathname}, payload) => {
+    const email = pathname
+      .split('/')
+      .slice(-1)
+      .find(Boolean);
+    let status = 200;
+
+    try {
+      await dataLib.read(BASE_DIR, email);
+    } catch (err) {
+      status = 204;
+    }
+
+    try {
+      await dataLib.remove(BASE_DIR, email);
+
+      return {metadata: {status}};
+    } catch (err) {
+      return createErrorResponse({status: 500, errors: [err], title: err.code});
+    }
+  },
+
+  /*
+   * Get a user
+   *
+   * /users/:email
+   */
+  get: async ({pathname}, payload) => {
+    const email = pathname
+      .split('/')
+      .slice(-1)
+      .find(Boolean);
+
+    try {
+      const data: User = await dataLib.read(BASE_DIR, email);
+
+      return {metadata: {status: 200}, payload: data};
+    } catch (err) {
+      return createErrorResponse({
+        errors: [err],
+        instance: pathname,
+        status: 404,
+        title: 'User not found',
+      });
+    }
+  },
+
   /*
    * Create a user.
    *
@@ -20,37 +72,54 @@ const userMethods: Service<User> = {
   post: async (req, payload) => {
     const password = validatePassword(payload.password);
     const email = validatePassword(payload.email);
-    const address = payload.address || null;
-    const name = payload.name || null;
+    const address = payload.address || '';
+    const name = payload.name || '';
     const invalidFields = [email, password].filter(hasErrors);
 
-    if (!invalidFields.length) {
-      const hashedPassword = createHash(password.value);
-      const id = createRandomString(10);
-      const user: User = {
-        address,
-        email: email.value,
-        id,
-        name,
-        password: hashedPassword,
-      };
-
-      try {
-        const result = await dataLib.create(BASE_DIR, id, user);
-
-        return {metadata: {status: 201}, payload: safeJSONParse(result)};
-      } catch (err) {
-        return createErrorResponse({
-          errors: [err],
-          status: 500,
-          title: err.code,
-        });
-      }
-    } else {
+    if (invalidFields.length) {
       return createErrorResponse({
         errors: invalidFields,
         status: 400,
         title: 'Invalid fields',
+      });
+    }
+
+    try {
+      await dataLib.read(BASE_DIR, email.value);
+
+      return createErrorResponse({
+        status: 409,
+        title: 'User exists',
+      });
+    } catch (err) {}
+
+    const hashedPassword = createHash(password.value);
+
+    if (!hashedPassword) {
+      return createErrorResponse({
+        status: 500,
+        title: `Couldn't hash password`,
+      });
+    }
+
+    const id = createRandomString(10);
+    const user: User = {
+      address,
+      email: email.value,
+      id,
+      name,
+      password: hashedPassword,
+    };
+
+    try {
+      const result = await dataLib.create(BASE_DIR, email.value, user);
+
+      return {metadata: {status: 201}, payload: result};
+    } catch (err) {
+      return createErrorResponse({
+        errors: [err],
+        status: 500,
+        title: err.code,
       });
     }
   },
