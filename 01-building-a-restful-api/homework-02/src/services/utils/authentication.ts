e4a66ec28ea2a4d482f9ebce1c5de9c10acb19bb
Larry Botha <larry@fixate.it>
2019-03-09 19:06:49 +0200

    add authentication helper

---
+++ 01-building-a-restful-api/homework-02/src/services/utils/authentication.ts
@@ -0,0 +1,44 @@
+import * as http from 'http';
+
+import {Token} from '../../types/entities/tokens';
+import {ResponseDataError} from '../../types/responses';
+
+import * as dataLib from '../../data';
+
+interface AuthenticationResult {
+  status: number;
+  title?: ResponseDataError['title'];
+  token?: Token;
+}
+
+type EvaluateAuthentication = (
+  headers: http.IncomingHttpHeaders,
+  tokenIdPathParam: string
+) => Promise<AuthenticationResult>;
+const evaluateAuthentication: EvaluateAuthentication = async (headers, tokenIdPathParam) => {
+  const errorResponse = {status: 401, title: 'Not authenticated'};
+
+  const [email, tokenId] = (headers.authorization || '')
+    .split(' ')
+    .slice(1)
+    .find(Boolean)
+    .split(':');
+
+  if (tokenId !== tokenIdPathParam) {
+    return errorResponse;
+  }
+
+  try {
+    const token: Token = await dataLib.read('tokens', tokenIdPathParam);
+
+    if (Date.now() > token.expires) {
+      return errorResponse;
+    }
+
+    return {status: 200, token};
+  } catch (err) {
+    return errorResponse;
+  }
+};
+
+export {evaluateAuthentication};
