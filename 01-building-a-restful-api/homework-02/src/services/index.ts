7eeff0ae00a77f2afbdb46a5d584222b63f3ef41
Larry Botha <larry@fixate.it>
2019-03-14 08:54:42 +0200

    add menu-items service

--- 01-building-a-restful-api/homework-02/src/services/index.ts
+++ 01-building-a-restful-api/homework-02/src/services/index.ts
@@ -1,6 +1,7 @@
 import {ResponseError, ResponseSuccess} from '../types/responses';
 import {Service, ServiceMethod} from '../types/services';
 
+import {menuItemsService as menuItems} from './menu-items';
 import {ping} from './ping';
 import {tokens} from './tokens';
 import {users} from './users';
@@ -9,6 +10,7 @@ interface Services {
   [key: string]: ServiceMethod;
 }
 const services: Services = {
+  'menu-items': menuItems,
   ping,
   tokens,
   users,
--- 01-building-a-restful-api/homework-02/src/services/menu-items.ts
+++ 01-building-a-restful-api/homework-02/src/services/menu-items.ts
@@ -1,1 +1,54 @@
+import {debuglog} from 'util';
+
 import {MenuItem} from '../types/entities/menu-items';
+import {Service} from '../types/services';
+
+import {menuItems} from '../fixtures/menu-items';
+import {createErrorResponse, createService} from './utils';
+import {evaluateAuthentication} from './utils/authentication';
+
+const debug = debuglog('menu-items');
+
+const menuItemsMethods: Service<MenuItem> = {
+  /**
+   * Get menu items
+   *
+   * /menu-items
+   *
+   * @param {object} request - request object
+   * @param {object} request.headers - request headers
+   * @param {string} request.pathname - request pathname
+   * @returns {Promise} - either a success response with menu items as payload, or an error response
+   */
+  get: async ({headers, pathname}) => {
+    const {status, title} = await evaluateAuthentication(headers);
+
+    if (!/^2\d{2}/.test(`${status}`)) {
+      debug(JSON.stringify(headers));
+
+      return createErrorResponse({
+        instance: pathname,
+        status,
+        title,
+      });
+    }
+
+    const pathParts = pathname.split('/').filter(Boolean);
+
+    if (pathParts.length === 1) {
+      return {metadata: {status: 200}, payload: menuItems};
+    } else {
+      return createErrorResponse({
+        instance: pathname,
+        status: 404,
+        title: 'Not found',
+      });
+    }
+  },
+};
+
+const allowedMethods = ['get'];
+
+const menuItemsService = createService(allowedMethods, menuItemsMethods);
+
+export {menuItemsService};
