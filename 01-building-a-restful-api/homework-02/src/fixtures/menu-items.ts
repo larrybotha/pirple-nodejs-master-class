7f9996bf63d7b2ec446022e9fb33ab83db9bd697
Larry Botha <larry@fixate.it>
2019-03-14 08:59:47 +0200

    add menu items

---
+++ 01-building-a-restful-api/homework-02/src/fixtures/menu-items.ts
@@ -0,0 +1,12 @@
+import {MenuItem} from '../types/entities/menu-items';
+
+const menuItems: MenuItem[] = [
+  {id: 0, name: 'pizza', price: 10},
+  {id: 1, name: 'mango', price: 9},
+  {id: 2, name: 'tartrazine', price: 11},
+  {id: 3, name: 'melon', price: 14},
+  {id: 4, name: 'tangerine', price: 13},
+  {id: 5, name: 'fish', price: 11},
+];
+
+export {menuItems};
--- 01-building-a-restful-api/homework-02/src/types/entities/menu-items.ts
+++ 01-building-a-restful-api/homework-02/src/types/entities/menu-items.ts
@@ -1,5 +1,5 @@
 interface MenuItem {
-  id: string;
+  id: number;
   name: string;
   price: number;
 }
