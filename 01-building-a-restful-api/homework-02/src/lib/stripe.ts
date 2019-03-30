7313500e47e3d6683349b7dc051b74752b65c592
Larry Botha <larry@fixate.it>
2019-03-30 13:56:00 +0200

    add stripe helpers

--- 01-building-a-restful-api/homework-02/src/lib/stripe.ts
+++ 01-building-a-restful-api/homework-02/src/lib/stripe.ts
@@ -1,19 +1,20 @@
 import * as https from 'https';
+import * as querystring from 'querystring';
 import * as util from 'util';
 
 import {config} from '../config';
 import {safeJSONParse} from '../helpers';
 
 const {stripe} = config.apis;
-const {secretKey} = stripe;
+const {apiKey, secretKey} = stripe;
 
 enum SourceType {
   AchCreditTransfer = 'ach_credit_transfer',
 }
 
 const baseRequestOptions: https.RequestOptions = {
-  headers: {Authorization: `Bearer ${secretKey}:`},
-  hostname: 'api.stripe.com/v1',
+  headers: {Authorization: `Bearer ${secretKey}`},
+  hostname: 'api.stripe.com',
   method: 'POST',
   protocol: 'https:',
 };
@@ -27,25 +28,28 @@ const baseRequestOptions: https.RequestOptions = {
  * @param {any} payload - data to send in request
  * @returns {undefined}
  */
-const createRequest = (options: https.RequestOptions, payload: any) => {
-  return new Promise((resolve, reject) => {
-    const stringifiedPayload = JSON.stringify(payload);
-    const requestOptions = {
-      ...options,
-      headers: {
-        // send data as form data
-        'Content-Type': 'application/x-www-form-urlencoded',
-        // set the content length of the payload
-        // This helps the receiving server know when the request's data has
-        // finished sending
-        'Content-length': Buffer.byteLength(stringifiedPayload),
-        ...options.headers,
-      },
-    };
+const createRequest = (options: https.RequestOptions, payload?: any) => {
+  // querystring.stringify formats data as required by servers parsing form data
+  const stringifiedPayload = querystring.stringify(payload);
+  const requestOptions = {
+    ...options,
+    headers: {
+      // send data as form data
+      'Content-Type': 'application/x-www-form-urlencoded',
+      // set the content length of the payload
+      // This helps the receiving server know when the request's data has
+      // finished sending
+      'Content-length': stringifiedPayload
+        ? Buffer.byteLength(stringifiedPayload)
+        : 0,
+      ...options.headers,
+    },
+  };
 
+  return new Promise((resolve, reject) => {
     const request = https.request(requestOptions, res => {
       const {statusCode} = res;
-      let data: string[];
+      let data: string[] = [];
       res.setEncoding('utf8');
 
       res.on('data', d => (data = data.concat(d)));
@@ -55,14 +59,16 @@ const createRequest = (options: https.RequestOptions, payload: any) => {
         const result = safeJSONParse(data.join(''));
 
         if (/^2/.test(`${statusCode}`)) {
-          resolve(result);
+          return resolve(result);
         } else {
-          reject(result);
+          return reject({statusCode, ...result});
         }
       });
     });
 
-    request.on('error', err => reject(err));
+    request.on('error', err => {
+      return reject(err);
+    });
 
     request.end(stringifiedPayload);
   });
@@ -98,11 +104,40 @@ const createSource: CreateSource = async email => {
   };
   const requestOptions: https.RequestOptions = {
     ...baseRequestOptions,
-    path: '/sources',
+    path: '/v1/sources',
   };
-  const result = await createRequest(requestOptions, payload);
 
-  return result;
+  try {
+    const result = await createRequest(requestOptions, payload);
+
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+type DeleteSource = (id: string) => Promise<any>;
+/**
+ * deleteSource
+ *
+ * Delete a source in Stripe
+ *
+ * @param {string} id - the id of the source to delete
+ * @returns {object} - either the new source, or an error
+ */
+const deleteSource: DeleteSource = async id => {
+  const requestOptions: https.RequestOptions = {
+    ...baseRequestOptions,
+    path: `/v1/sources/${id}`,
+  };
+
+  try {
+    const result = await createRequest(requestOptions);
+
+    return result;
+  } catch (err) {
+    throw err;
+  }
 };
 
 type CreateCustomer = (options: {
@@ -122,12 +157,15 @@ const createCustomer: CreateCustomer = async options => {
   const payload = {...options};
   const requestOptions: https.RequestOptions = {
     ...baseRequestOptions,
-    path: '/customers',
+    path: '/v1/customers',
   };
-  debugger;
-  const result = await createRequest(requestOptions, payload);
+  try {
+    const result = await createRequest(requestOptions, payload);
 
-  return result;
+    return result;
+  } catch (err) {
+    throw err;
+  }
 };
 
 type UpdateCustomer = (
@@ -147,11 +185,48 @@ const updateCustomer: UpdateCustomer = async (id, options) => {
   const payload = {...options};
   const requestOptions: https.RequestOptions = {
     ...baseRequestOptions,
-    path: `/customers/${id}`,
+    path: `/v1/customers/${id}`,
   };
-  const result = await createRequest(requestOptions, payload);
 
-  return result;
+  try {
+    const result = await createRequest(requestOptions, payload);
+
+    return result;
+  } catch (err) {
+    throw err;
+  }
 };
 
-export {createCustomer, createSource, updateCustomer};
+type DeleteCustomer = (id: string) => Promise<any>;
+/**
+ * deleteCustomer
+ *
+ * Delete an existing customer in Stripe
+ *
+ * @param {string} id - Stripe id of customer to update
+ * @param {object} options - options to update on the user
+ * @returns {object} - either the customer from Stripe, or an error
+ */
+const deleteCustomer: DeleteCustomer = async id => {
+  const requestOptions: https.RequestOptions = {
+    ...baseRequestOptions,
+    method: 'DELETE',
+    path: `/v1/customers/${id}`,
+  };
+
+  try {
+    const result = await createRequest(requestOptions);
+
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export {
+  createCustomer,
+  createSource,
+  deleteCustomer,
+  deleteSource,
+  updateCustomer,
+};
