b87f5b157a5bb613508c5536b5b94e4f9d5a29fe
Larry Botha <larry@fixate.it>
2019-03-31 14:49:02 +0200

    add helper for creating requests

---
+++ 01-building-a-restful-api/homework-02/src/helpers/create-request.ts
@@ -0,0 +1,62 @@
+import * as https from 'https';
+import * as querystring from 'querystring';
+
+import {safeJSONParse} from '../helpers';
+
+/**
+ * createRequest
+ *
+ * Helper for creating requests
+ *
+ * @param {object} - request options for this request
+ * @param {any} payload - data to send in request
+ * @returns {undefined}
+ */
+const createRequest = (
+  options: https.RequestOptions = {},
+  payload?: any
+): Promise<any> => {
+  // querystring.stringify formats data as required by servers parsing form data
+  const stringifiedPayload = querystring.stringify(payload);
+  const requestOptions = {
+    ...options,
+    headers: {
+      // set the content length of the payload
+      // This helps the receiving server know when the request's data has
+      // finished sending
+      'Content-length': stringifiedPayload
+        ? Buffer.byteLength(stringifiedPayload)
+        : 0,
+      ...options.headers,
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    const request = https.request(requestOptions, res => {
+      const {statusCode} = res;
+      let data: string[] = [];
+      res.setEncoding('utf8');
+
+      res.on('data', d => (data = data.concat(d)));
+
+      res.on('end', (d: string) => {
+        data = data.concat(d);
+        const result = safeJSONParse(data.join(''));
+
+        if (/^2/.test(`${statusCode}`)) {
+          return resolve(result);
+        } else {
+          return reject({statusCode, ...result});
+        }
+      });
+    });
+
+    request.on('error', err => {
+      return reject(err);
+    });
+
+    request.end(stringifiedPayload);
+  });
+};
+
+export {createRequest};
