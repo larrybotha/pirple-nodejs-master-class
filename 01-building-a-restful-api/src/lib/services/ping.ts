89b602c450af3e9ee0af6e3937715120ef76e3b7
Larry Botha <larry@fixate.it>
2019-04-05 09:19:00 +0200

    rewrite ping as service

--- 01-building-a-restful-api/src/lib/services/ping.ts
+++ 01-building-a-restful-api/src/lib/services/ping.ts
@@ -1,6 +1,18 @@
 import {Handler} from '../types';
 
-// a ping handler purely for clients to evaluate whether the server is up or not
-export const ping: Handler = (data, cb) => {
-  cb(200, {status: 'running'});
+import {createServiceRouter} from './utils';
+
+interface PingMethods {
+  get: Handler;
+}
+
+const pingMethods: {[key: string]: Handler} = {
+  get: (_, cb) => {
+    cb(200, {status: 'running'});
+  },
 };
+
+const allowedMethods = ['get'];
+const ping = createServiceRouter(allowedMethods, pingMethods);
+
+export {ping};
