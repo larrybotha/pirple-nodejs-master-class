fac960f39e1880d0b2a39153089eed6411557600
Larry Botha <larry@fixate.it>
2019-06-01 16:45:54 -0500

    add performance measurement

--- 01-building-a-restful-api/src/lib/services/api/tokens.ts
+++ 01-building-a-restful-api/src/lib/services/api/tokens.ts
@@ -1,3 +1,6 @@
+import {performance, PerformanceObserver} from 'perf_hooks';
+import {debuglog} from 'util';
+
 import dataLib from '../../data';
 import helpers from '../../helpers';
 import {Handler} from '../../types';
@@ -30,6 +33,65 @@ interface TokenMethods {
   put: Handler<TokenPutPayload>;
   [key: string]: any;
 }
+
+const debug = debuglog('performance');
+const observer = new PerformanceObserver((items, obs) => {
+  const entries = items.getEntriesByType('measure');
+
+  entries.map(entry => {
+    debug('\x1b[36m%s\x1b[0m', `${entry.name} ${entry.duration}`);
+  });
+});
+observer.observe({entryTypes: ['measure']});
+
+enum PerformanceMarks {
+  START_INPUT_VALIDATON = 'start input validation',
+  END_INPUT_VALIDATION = 'end input validation',
+  START_CREATE_TOKEN = 'start create token',
+  END_CREATE_TOKEN = 'end create token',
+  START_USER_LOOKUP = 'start user lookup',
+  END_USER_LOOKUP = 'end user lookup',
+  START_HASH_PASSWORD = 'start hash password',
+  END_HASH_PASSWORD = 'end hash password',
+  START_TOKEN_STORAGE = 'start token storage',
+  END_TOKEN_STORAGE = 'end token storage',
+}
+
+enum PerformanceMeasures {
+  START_TO_END = 'start to end',
+  VALIDATING_INPUT = 'validating input',
+  USER_LOOKUP = 'user lookup',
+  PASSWORD_HASHING = 'password hashing',
+  TOKEN_CREATION = 'token data creation',
+  TOKEN_STORAGE = 'token storage',
+}
+
+const performanceMap: {[key: string]: string[]} = {
+  [PerformanceMeasures.START_TO_END]: [
+    PerformanceMarks.START_INPUT_VALIDATON,
+    PerformanceMarks.END_TOKEN_STORAGE,
+  ],
+  [PerformanceMeasures.VALIDATING_INPUT]: [
+    PerformanceMarks.START_INPUT_VALIDATON,
+    PerformanceMarks.END_INPUT_VALIDATION,
+  ],
+  [PerformanceMeasures.USER_LOOKUP]: [
+    PerformanceMarks.START_USER_LOOKUP,
+    PerformanceMarks.END_USER_LOOKUP,
+  ],
+  [PerformanceMeasures.PASSWORD_HASHING]: [
+    PerformanceMarks.START_HASH_PASSWORD,
+    PerformanceMarks.END_HASH_PASSWORD,
+  ],
+  [PerformanceMeasures.TOKEN_CREATION]: [
+    PerformanceMarks.START_CREATE_TOKEN,
+    PerformanceMarks.END_CREATE_TOKEN,
+  ],
+  [PerformanceMeasures.TOKEN_STORAGE]: [
+    PerformanceMarks.START_TOKEN_STORAGE,
+    PerformanceMarks.END_TOKEN_STORAGE,
+  ],
+};
 
 const getTokenIdFromPath = (path: string): string => {
   const parts = path.split('/');
@@ -113,6 +175,7 @@ const tokenMethods: TokenMethods = {
    * only authenticated users may create a token
    */
   post: async ({payload}, cb) => {
+    performance.mark(PerformanceMarks.START_INPUT_VALIDATON);
     const phone = validatePhone(payload.phone);
     const password = validatePassword(payload.password);
     const fields = [phone, password];
@@ -120,12 +183,17 @@ const tokenMethods: TokenMethods = {
       field => Boolean(field) && Boolean(field.error)
     );
     const isValid = invalidFields.length === 0;
+    performance.mark(PerformanceMarks.END_INPUT_VALIDATION);
 
     if (isValid) {
       try {
+        performance.mark(PerformanceMarks.START_USER_LOOKUP);
         const user: User = await dataLib.read('users', phone);
+        performance.mark(PerformanceMarks.END_USER_LOOKUP);
 
+        performance.mark(PerformanceMarks.START_HASH_PASSWORD);
         const hashedPassword = helpers.hash(payload.password);
+        performance.mark(PerformanceMarks.END_HASH_PASSWORD);
 
         if (!hashedPassword) {
           return cb(500, {error: `Couldn't hash user's password`});
@@ -135,6 +203,7 @@ const tokenMethods: TokenMethods = {
           return cb(400, {error: 'username or password incorrect'});
         }
 
+        performance.mark(PerformanceMarks.START_CREATE_TOKEN);
         const tokenId = helpers.createRandomString(20);
         const expires = Date.now() + 1000 * 60 * 60;
         const token: Token = {
@@ -142,8 +211,16 @@ const tokenMethods: TokenMethods = {
           id: tokenId,
           phone,
         };
+        performance.mark(PerformanceMarks.END_CREATE_TOKEN);
 
+        performance.mark(PerformanceMarks.START_TOKEN_STORAGE);
         const tokenData: Token = await dataLib.create('tokens', tokenId, token);
+        performance.mark(PerformanceMarks.END_TOKEN_STORAGE);
+
+        Object.keys(performanceMap).map(key => {
+          const [start, end] = performanceMap[key];
+          performance.measure(key, start, end);
+        });
 
         cb(201, tokenData);
       } catch (err) {
