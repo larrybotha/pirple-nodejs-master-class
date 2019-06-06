207d137120d3a62d93d6f4c7629e7e75114e0203
Larry Botha <larry@fixate.it>
2019-06-05 23:25:33 -0500

    add cluster

--- 01-building-a-restful-api/src/index.ts
+++ 01-building-a-restful-api/src/index.ts
@@ -1,15 +1,27 @@
+import * as cluster from 'cluster';
+import * as os from 'os';
+
 import cli from './lib/cli';
 import server from './lib/server';
 import workers from './lib/workers';
 
 const init = () => {
-  // start the server
-  server.init();
+  if (cluster.isMaster) {
+    process.nextTick(cli.init);
 
-  // start the workers
-  workers.init();
+    Array.from({length: os.cpus().length}).map(() => {
+      cluster.fork();
+    });
+  } else {
+    // start the server
+    server.init();
 
-  process.nextTick(cli.init);
+    // start the workers
+    workers.init();
+
+    // tslint:disable-next-line
+    console.log(`worker ${process.pid} died`);
+  }
 };
 
 const app = {init};
