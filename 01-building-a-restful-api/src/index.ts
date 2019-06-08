175131e90004ab8656554d0d8ff867d65ec1a422
Larry Botha <larry@fixate.it>
2019-06-08 16:11:25 -0500

    split out init functions

--- 01-building-a-restful-api/src/index.ts
+++ 01-building-a-restful-api/src/index.ts
@@ -5,13 +5,23 @@ import cli from './lib/cli';
 import server from './lib/server';
 import workers from './lib/workers';
 
-const init = () => {
+const startSingleProcesses = () => {
+  // start cli
+  process.nextTick(cli.init);
+
+  // start the workers
+  workers.init();
+};
+
+const startMultiProcesses = () => {
+  // start the server
+  server.init();
+};
+
+const startCluster = () => {
   // if on master cluster, start cli and workers
   if (cluster.isMaster) {
-    process.nextTick(cli.init);
-
-    // start the workers
-    workers.init();
+    startSingleProcesses();
 
     // create a forked cluster for each core available
     Array.from({length: os.cpus().length}).map(() => {
@@ -20,11 +30,19 @@ const init = () => {
   }
   // if not on master, serve requests on all cores
   else {
-    // start the server
-    server.init();
+    startMultiProcesses();
 
     // tslint:disable-next-line
     console.log(`worker ${process.pid} died`);
+  }
+};
+
+const init = () => {
+  if (process.env.NODE_ENV === 'production') {
+    startCluster();
+  } else {
+    startSingleProcesses();
+    startMultiProcesses();
   }
 };
 
